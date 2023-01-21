720cdce3c56fe7df989092f920ed9640e980c8b5
Mirco Forcella <forcella1996@gmail.com>
2023-01-21 17:18:27 +0100

    Initial commit

---
+++ src/it/develhop/encapsulation/Start.java
@@ -0,0 +1,35 @@
+package it.develhop.encapsulation;
+
+import it.develhop.encapsulation.entities.Person;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Person person = new Person();
+
+        System.out.println("Your name: ");
+
+        person.setName(scanner.nextLine());
+
+        System.out.println("Your surname: ");
+        person.setSurname(scanner.nextLine());
+
+        System.out.println("Your height in meter: ");
+        try{
+        person.setHeight(scanner.nextDouble());
+        } catch (InputMismatchException e) {
+            System.out.println("Error, try using comma (,)");
+        }
+
+        System.out.println("Your age: ");
+        person.setAge(scanner.nextInt());
+        scanner.close();
+
+        System.out.println(String.format("Person name: %s \nSurname: %s \nHeight: %.2f \nAge: %d",
+                person.getName(), person.getSurname(),person.getHeight(),person.getAge()));
+    }
+}
